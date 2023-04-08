278df15f3254548bc8127f3ec90d407758dc9485
miha04635 <124861244+miha04635@users.noreply.github.com>
2023-04-08 23:03:05 +0500

    1

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	const container = document.querySelector('.brands');
 	const showMoreButton = document.querySelector('.brands__show-more--bth');
 	const icon = document.querySelector('.icon');
+	const iconDevice = document.querySelector('.Device__icon');
+	const containerDevice = document.querySelector('.Device');
+	const showMoreButtonDevice = document.querySelector('.Device__show-more--bth');
 
 	if (window.matchMedia("(max-width: 700px)").matches) {
 		let swiper = new Swiper(".mySwiper", {
@@ -32,6 +35,26 @@ showMoreButton.onclick = function() {
 	icon.classList.toggle('icon__content');
 	container.classList.toggle('expanded');
 }
+
+//
+
+const showMore = (event) => {
+	if (containerDevice?.classList.contains("expanded")) {
+		showMoreButtonDevice.textContent = "Показать всё";
+	  
+	} else {
+		showMoreButtonDevice.textContent = "Скрыть";
+	}
+  
+  };
+  
+  showMoreButtonDevice.addEventListener("click", handleClick);
+  
+  
+  showMoreButtonDevice.onclick = function() {
+	  iconDevice.classList.toggle('icon__content');
+	  containerDevice.classList.toggle('expanded');
+  }
 
 });
 
